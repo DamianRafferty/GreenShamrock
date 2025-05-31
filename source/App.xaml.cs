75eccbfb9d49fd6c36d06e90b06da9a06dcb7def
DamianRafferty <damian.rafferty@pulseplm.com>
2025-05-31 15:53:45 +0100

    Set default application theme

--- source/App.xaml.cs
+++ source/App.xaml.cs
@@ -1,8 +1,14 @@
 ﻿using System.Windows;
+using DevExpress.Xpf.Core;
 
 namespace Pulse.PLMSuite.Modeller
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ApplicationThemeHelper.ApplicationThemeName = Theme.Office2019WhiteName;
+            base.OnStartup(e);
+        }
     }
 }
